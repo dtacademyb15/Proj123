348cc434261078a4f3de8690dd06a894d6b752bd
Duotech B15 <duotechb15@doe.org>
2024-07-10 19:11:41 -0500

    initial

---
+++ src/TestClass.java
@@ -0,0 +1,10 @@
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Line 1");
+        System.out.println("Line3");
+        System.out.println("Line 5");
+        System.out.println("Line 9");
+    }
+}
